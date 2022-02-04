889c205c7f951264cf150939f49774cf1e1cd7fe
sir-ct <davidsirct@gmail.com>
2022-02-04 10:47:08 +0100

    more changes

--- public/js/mobilenav.js
+++ public/js/mobilenav.js
@@ -1,10 +1,16 @@
 function opennav(){
-    let mnav = document.getElementById("mobilenav")
-    let navbar = document.getElementById("navbar")
+  let mnav = document.getElementById("mobilenav")
+  let navbar = document.getElementById("navbar")
+  let welcometxt = document.getElementById("welcome-txt")
+  let welcomemnu = document.getElementById("welcome-mnu")
 
 mnav.addEventListener("click", ()=>{
-  navbar.classList.toggle("show")
-  mnav.classList.toggle("show")
+navbar.classList.toggle("show")
+mnav.classList.toggle("show")
+})
+
+welcometxt.addEventListener("click", ()=>{
+welcomemnu.classList.toggle("show")
 })
 }
 window.addEventListener("load", opennav)
