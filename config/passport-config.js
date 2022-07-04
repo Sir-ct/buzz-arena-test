c568b7607e7cd3f5cfae82c6fe2f1c2fc09fd7a6
sir-ct <davidsirct@gmail.com>
2022-07-04 08:03:15 +0100

    updating google auth callback to buzzarena.net

--- config/passport-config.js
+++ config/passport-config.js
@@ -30,7 +30,7 @@ function initialize(passport){
     passport.use(new GoogleStrategy({
         clientID:    process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:5000/auth/google/callback",
+        callbackURL: "https://buzzarena.net/auth/google/callback",
         passReqToCallback   : true
       },
       async (request, accessToken, refreshToken, profile, done) => {
